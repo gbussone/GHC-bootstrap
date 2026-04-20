05a8beb39ef206871b5448780bd550cf034a41d6
Grégoire BUSSONE <gregoire.bussone@orange.fr>
2026-04-20 15:08:52 +0200

    Use old nixpkgs commit directly

--- flake.nix
+++ flake.nix
@@ -62,6 +62,10 @@
       url = "github:NixOS/nixpkgs/0.14";
       flake = false;
     };
+    nixpkgs_2ad6c9c6c215d48bd1524e8f281ede8bfea1e2dd = {
+      url = "github:NixOS/nixpkgs/2ad6c9c6c215d48bd1524e8f281ede8bfea1e2dd";
+      flake = false;
+    };
   };
 
   outputs =
@@ -91,6 +95,7 @@
       nixpkgs_15_09,
       nixpkgs_last_glibc_2_13,
       nixpkgs_0_14,
+      nixpkgs_2ad6c9c6c215d48bd1524e8f281ede8bfea1e2dd,
     }:
     let
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
@@ -154,27 +159,92 @@
       pkgs32_last_glibc_2_13 = (import nixpkgs_last_glibc_2_13' { system = "i686-linux"; }).pkgs;
       pkgs_0_14 = import nixpkgs_0_14 { system = "x86_64-linux"; };
       pkgs32_0_14 = import nixpkgs_0_14 { system = "i686-linux"; };
-      binutils_2_20_1 =
-        with pkgs_0_14.binutils;
-        let
-          version = "2.20.1";
-        in
-        pkgs_0_14.stdenv.mkDerivation {
-          name = "binutils-${version}";
-          src = pkgs.fetchurl {
-            url = "mirror://gnu/binutils/binutils-${version}.tar.bz2";
-            hash = "sha256-cdN8lkUTM8XAuEsXAWn9yxOLuyc5fcBigZBdlxfI7WQ=";
-          };
-          inherit
-            buildInputs
-            configureFlags
-            enableParallelBuilding
-            NIX_CFLAGS_COMPILE
-            noSysDirs
-            patches
-            preConfigure
-            ;
-        };
+      nixpkgs_2ad6c9c6c215d48bd1524e8f281ede8bfea1e2dd' = pkgs.applyPatches {
+        name = "nixpkgs";
+        src = nixpkgs_2ad6c9c6c215d48bd1524e8f281ede8bfea1e2dd;
+        patches = [
+          ./nixpkgs_2ad6c9c6c215d48bd1524e8f281ede8bfea1e2dd/binutils_2_20_1.patch
+          ./nixpkgs_2ad6c9c6c215d48bd1524e8f281ede8bfea1e2dd/binutils_2_20_51.patch
+          ./nixpkgs_2ad6c9c6c215d48bd1524e8f281ede8bfea1e2dd/bootstrap_tools.patch
+          ./nixpkgs_2ad6c9c6c215d48bd1524e8f281ede8bfea1e2dd/cloog_ppl_0_15_9.patch
+          ./nixpkgs_2ad6c9c6c215d48bd1524e8f281ede8bfea1e2dd/curl_7_21_0.patch
+          ./nixpkgs_2ad6c9c6c215d48bd1524e8f281ede8bfea1e2dd/libsigsegv_2_8.patch
+          ./nixpkgs_2ad6c9c6c215d48bd1524e8f281ede8bfea1e2dd/libssh2_1_2_6.patch
+          ./nixpkgs_2ad6c9c6c215d48bd1524e8f281ede8bfea1e2dd/mpc_0_8_2.patch
+          ./nixpkgs_2ad6c9c6c215d48bd1524e8f281ede8bfea1e2dd/openssl_1_0_0.patch
+          ./nixpkgs_2ad6c9c6c215d48bd1524e8f281ede8bfea1e2dd/patchelf_0_5.patch
+          ./nixpkgs_2ad6c9c6c215d48bd1524e8f281ede8bfea1e2dd/xz_5_0_0.patch
+          ./nixpkgs_2ad6c9c6c215d48bd1524e8f281ede8bfea1e2dd/zlib_1_2_5.patch
+        ];
+        prePatch = ''
+          cp ${
+            pkgs.fetchurl {
+              url = "http://nixos.org/tarballs/binutils-2.20.51-pre-20100901.tar.bz2";
+              sha256 = "1872fdnbnq5z5svq7mvc0vyyad8pknwvx2glxq1bbk0xv7arp72y";
+            }
+          } pkgs/development/tools/misc/binutils/binutils-2.20.51-pre-20100901.tar.bz2
+          cp ${
+            pkgs.fetchurl {
+              url = "http://nixos.org/tarballs/stdenv-linux/x86_64/r23302/bootstrap-tools.cpio.bz2";
+              sha256 = "0w89kqhx47yl0jifp2vffp073pyrqha5f312kp971smi4h41drna";
+            }
+          } pkgs/stdenv/linux/bootstrap/x86_64/bootstrap-tools.cpio.bz2
+          cp ${
+            pkgs.fetchurl {
+              url = "https://ftp2.osuosl.org/pub/clfs/conglomeration/cloog-ppl/cloog-ppl-0.15.9.tar.gz";
+              sha256 = "19a2n75k3d3n8llng25f2g88lpvd4zn0lm073rkndjw6l6yd8m4c";
+            }
+          } pkgs/development/libraries/cloog-ppl/cloog-ppl-0.15.9.tar.gz
+          cp ${
+            pkgs.fetchurl {
+              url = "https://curl.se/download/archeology/curl-7.21.0.tar.gz";
+              hash = "sha256-s+IEfG9w6zIVV6+YCpVU8KmPsSLZY28cmIMyYu7Y3h0=";
+            }
+          } pkgs/tools/networking/curl/curl-7.21.0.tar.gz
+          cp ${
+            pkgs.fetchurl {
+              url = "http://www.libssh2.org/download/libssh2-1.2.6.tar.gz";
+              sha256 = "00f6hw972v7jd0rrdr6kx5cn7pa1spyx8xgc7vhjydksgsig3f8b";
+            }
+          } pkgs/development/libraries/libssh2/libssh2-1.2.6.tar.gz
+          cp ${
+            pkgs.fetchurl {
+              url = "https://www.multiprecision.org/downloads/mpc-0.8.2.tar.gz";
+              sha256 = "1iw0ag28l5r88k7kpn6i89rqn3yhk2irqzk0d1mlb1la3paghydf";
+            }
+          } pkgs/development/libraries/mpc/mpc-0.8.2.tar.gz
+          cp ${
+            pkgs.fetchurl {
+              url = "http://www.openssl.org/source/openssl-1.0.0c.tar.gz";
+              sha256 = "1sq4sswyjxnr08lyjcafwdha6j5jd2b48vxfg48kdapdwdnv6cgp";
+            }
+          } pkgs/development/libraries/openssl/openssl-1.0.0c.tar.gz
+          cp ${
+            pkgs.fetchurl {
+              url = "http://nixos.org/releases/patchelf/patchelf-0.5/patchelf-0.5.tar.bz2";
+              sha256 = "24b9a850af45e1a277e234b9eb090b52305a2e1c6b02addeb3ae98b4b49d37ce";
+            }
+          } pkgs/development/tools/misc/patchelf/patchelf-0.5.tar.bz2
+          cp ${
+            pkgs.fetchurl {
+              url = "http://tukaani.org/xz/xz-5.0.0.tar.bz2";
+              sha256 = "1n2nc00d83di0jl5d9qwpngkmlk3wqhs4w9q2ah680v9qijrxa27";
+            }
+          } pkgs/tools/compression/xz/xz-5.0.0.tar.bz2
+          cp ${
+            pkgs.fetchurl {
+              url = "https://zlib.net/fossils/zlib-1.2.5.tar.gz";
+              sha256 = "0n7rlgvjn73pyil3s1l6p77m7wkc809n934rnzxv1b1za4pfar30";
+            }
+          } pkgs/development/libraries/zlib/zlib-1.2.5.tar.gz
+        '';
+      };
+      pkgs_2ad6c9c6c215d48bd1524e8f281ede8bfea1e2dd =
+        import nixpkgs_2ad6c9c6c215d48bd1524e8f281ede8bfea1e2dd'
+          { system = "x86_64-linux"; };
+      pkgs32_2ad6c9c6c215d48bd1524e8f281ede8bfea1e2dd =
+        import nixpkgs_2ad6c9c6c215d48bd1524e8f281ede8bfea1e2dd'
+          { system = "i686-linux"; };
     in
     {
       packages.x86_64-linux = {
@@ -220,9 +290,9 @@
         ghc_6_6_1 = pkgs.callPackage ./ghc_6_6_1 {
           perl = pkgs_last_glibc_2_13.perl58;
           gcc = pkgs_20_03.wrapCCWith {
-            cc = pkgs_last_glibc_2_13.gcc;
+            cc = pkgs_last_glibc_2_13.gcc.gcc;
             bintools = pkgs_20_03.wrapBintoolsWith {
-              bintools = binutils_2_20_1;
+              bintools = pkgs_2ad6c9c6c215d48bd1524e8f281ede8bfea1e2dd.binutils_release;
               libc = pkgs_15_09.glibc;
             };
           };
